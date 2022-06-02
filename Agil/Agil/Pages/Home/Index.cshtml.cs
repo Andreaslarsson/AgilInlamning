46a31de75b51a95fdb50e63c27d58a84350783db
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-06-02 15:56:05 +0200

    Start på redigera och radera knappar till mina annonser #63

---
+++ Agil/Agil/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Agil.Data;
+using Agil.Models;
+
+namespace Agil.Pages.Home
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Agil.Data.ApplicationDbContext _context;
+
+        public IndexModel(Agil.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Item { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Items != null)
+            {
+                Item = await _context.Items.ToListAsync();
+            }
+        }
+    }
+}
