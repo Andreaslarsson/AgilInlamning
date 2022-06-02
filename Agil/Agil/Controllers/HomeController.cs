009677fb5cd902217a8d48de06a97175dc2c7d9c
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-06-02 12:15:55 +0200

    Mina annonser #59

--- Agil/Agil/Controllers/HomeController.cs
+++ Agil/Agil/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Agil.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Agil.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Services.WebsiteHandler _websiteHandler;
+
+        public HomeController(WebsiteHandler websiteHandler)
+        {
+            _websiteHandler = websiteHandler;
+        }
         public IActionResult Items()
         {
             return View();
+        }
+        public IActionResult MyPostedAdvertisement(string userId)
+        {
+            var myPostedAdvertisements = _websiteHandler.MyPostedAdvertisement(userId);
+            return View(myPostedAdvertisements);
         }
     }
 }
