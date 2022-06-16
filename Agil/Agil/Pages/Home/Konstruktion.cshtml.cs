a1d4174e26390fed4fd90f7eb4e9368b931cc7ec
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-06-16 12:19:05 +0200

    Under konstruktion

---
+++ Agil/Agil/Pages/Home/Konstruktion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Agil.Pages.Home
+{
+    public class BevakningarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
